9ee3896e6e228af22de9683f3744a75671eb0be1
Tim Trippel <ttrippel@google.com>
2024-10-07 21:18:31 -0700

    [test] add sival SKU integration tests
    This updates the PA loadtest and integration test script to enable
    testing the sival SKU provisioning sequence that now includes invoking
    the DeriveSymmetricKey function on the PA and SPM.
    
    Part of refactoring the loadtest will enable testing additional SKUs in
    the future as well.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -10,22 +10,22 @@ readonly REPO_TOP=$(git rev-parse --show-toplevel)
 # Parse command line options.
 for i in "$@"; do
   case $i in
-    # -c option: Only build/deploy container images, not raw binaries.
-    # Saves time when running this script if not permanently deploying infra.
-    -c|--containers-only)
-      export CONTAINERS_ONLY="yes"
-      shift
-      ;;
-    # -d option: Activate debug mode, which will not tear down containers if
-    # there is a failure so the failure can be inspected.
-    -d|--debug)
-      export DEBUG="yes"
-      shift
-      ;;
-    *)
-      echo "Unknown option $i"
-      exit 1
-      ;;
+  # -c option: Only build/deploy container images, not raw binaries.
+  # Saves time when running this script if not permanently deploying infra.
+  -c | --containers-only)
+    export CONTAINERS_ONLY="yes"
+    shift
+    ;;
+  # -d option: Activate debug mode, which will not tear down containers if
+  # there is a failure so the failure can be inspected.
+  -d | --debug)
+    export DEBUG="yes"
+    shift
+    ;;
+  *)
+    echo "Unknown option $i"
+    exit 1
+    ;;
   esac
 done
 
@@ -40,9 +40,6 @@ else
 fi
 bazelisk build --stamp //release:softhsm_dev
 
-# Deploy the provisioning appliance services
-. ${REPO_TOP}/config/dev/env/spm.env
-
 # Register trap to shutdown containers before exit.
 # Teardown containers. This currently does not remove the container volumes.
 shutdown_containers() {
@@ -53,23 +50,33 @@ if [ -z "${DEBUG}" ]; then
   trap shutdown_containers EXIT
 fi
 
+# Deploy the provisioning appliance binaries and services.
+. ${REPO_TOP}/config/dev/env/spm.env
 ${REPO_TOP}/config/dev/deploy.sh ${REPO_TOP}/bazel-bin/release
 
+# Initialize and configure the HSM.
 bazelisk run //src/spm:spmutil -- \
-    --hsm_pw="${SPM_HSM_PIN_USER}" \
-    --hsm_so="${OPENTITAN_VAR_DIR}/softhsm2/libsofthsm2.so" \
-    --hsm_type=0 \
-    --hsm_slot=0 \
-    --force_keygen \
-    --gen_kg \
-    --gen_kca \
-    --load_low_sec_ks \
-    --low_sec_ks="0x23df79a8052010ef6e3d49255b606f871cff06170247c1145ebb71ad23834061" \
-    --load_high_sec_ks \
-    --high_sec_ks="0xaba9d5616e5a7c18b9a41d8a22f42d4dc3bafa9ca1fad01e404e708b1eab21fd" \
-    --ca_outfile="${OPENTITAN_VAR_DIR}/spm/config/certs/NuvotonTPMRootCA0200.cer"
+  --hsm_pw="${SPM_HSM_PIN_USER}" \
+  --hsm_so="${OPENTITAN_VAR_DIR}/softhsm2/libsofthsm2.so" \
+  --hsm_type=0 \
+  --hsm_slot=0 \
+  --force_keygen \
+  --gen_kg \
+  --gen_kca \
+  --load_low_sec_ks \
+  --low_sec_ks="0x23df79a8052010ef6e3d49255b606f871cff06170247c1145ebb71ad23834061" \
+  --load_high_sec_ks \
+  --high_sec_ks="0xaba9d5616e5a7c18b9a41d8a22f42d4dc3bafa9ca1fad01e404e708b1eab21fd" \
+  --ca_outfile="${OPENTITAN_VAR_DIR}/spm/config/certs/NuvotonTPMRootCA0200.cer"
 
-bazelisk run //src/pa:loadtest -- \
+# Run the loadtest on each SKU.
+SKUS=("tpm_1" "sival")
+for sku in "${SKUS[@]}"; do
+  echo "Running PA loadtest with sku: ${sku} ..."
+  bazelisk run //src/pa:loadtest -- \
     --pa_address="localhost:5001" \
+    --sku="${sku}" \
+    --sku_auth="test_password" \
     --parallel_clients=10 \
     --total_calls_per_client=10
+done
