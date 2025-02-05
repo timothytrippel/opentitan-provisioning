e8d0ddf80e9cd9e3da93c570e87ddc7a6318259a
Tim Trippel <ttrippel@google.com>
2025-02-05 11:41:41 -0800

    [fpga] check in hyper310 and cw340 bitstreams for testing
    This checks in OT FPGA bitstreams for testing E2E provisioning flows. We
    check in the bitstreams to simplify dependency management.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

---
+++ third_party/lowrisc/ot_bitstreams/build-ot-bitstreams.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# This script make it easy to build an check-in OT FPGA bitstreams for E2E 
+# testing.
+#
+# This script should be run from the root of this repo on a machine that also
+# has the https://github.com/lowRISC/opentitan repo cloned and their system
+# configured to build bitstreams.
+
+set -e
+
+if [[ $# != 1 ]]; then
+  echo "Usage: ./third_party/lowrisc/ot_bitstreams/build-ot-bitstreams.sh <OT repo top path>"
+  exit 1
+fi
+
+OT_REPO_TOP=$1
+
+_OT_REPO_BRANCH="Earlgrey-PROD-A2-M6-ROM-RC1"
+_PROVISIONING_REPO_TOP=$(pwd)
+_FPGAS=("hyper310" "cw340")
+_CP_SKUS=("emulation")
+_BITSTREAM_PATH="bazel-bin/hw/bitstream/universal/splice.bit"
+
+# Move to OT repo to build the bitstreams. This avoids polluting this project's
+# bazel WORKSPACE just to build a couple of bitstream assets that are checked in
+# to the repo.
+cd "$OT_REPO_TOP"
+git checkout $_OT_REPO_BRANCH
+echo "Performing builds from: $(pwd)."
+
+# Builds bitstreams for testing CP provisioning flows.
+for fpga in "${_FPGAS[@]}"; do
+  for sku in "${_CP_SKUS[@]}"; do
+    echo "Building CP ${fpga} bitstream for ${sku} ..."
+    bazelisk build \
+      --//hw/bitstream/universal:otp=//hw/ip/otp_ctrl/data/earlgrey_skus/"$sku":otp_img_test_unlocked0_manuf_empty \
+      --//hw/bitstream/universal:env=//hw/top_earlgrey:fpga_"$fpga"_rom_with_fake_keys \
+      //hw/bitstream/universal:splice
+    cp -f "${_BITSTREAM_PATH}" "${_PROVISIONING_REPO_TOP}/third_party/lowrisc/ot_bitstreams/cp_${fpga}.bit"
+    chmod -x "${_PROVISIONING_REPO_TOP}/third_party/lowrisc/ot_bitstreams/cp_${fpga}.bit"
+  done
+done
+
+cd "$_PROVISIONING_REPO_TOP"
