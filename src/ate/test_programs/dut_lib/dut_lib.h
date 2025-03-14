384db13e69a5fe191eb86f76616f65fda7465eb8
Tim Trippel <ttrippel@google.com>
2025-03-14 13:13:32 -0700

    [dut_lib] cleanup class definition
    This cleans up the class definition to be compliant with Google C++
    Style Guide we follow in this project.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- src/ate/test_programs/dut_lib/dut_lib.h
+++ src/ate/test_programs/dut_lib/dut_lib.h
@@ -12,38 +12,47 @@ namespace provisioning {
 namespace test_programs {
 
 class DutLib {
+ public:
+  /**
+   * Factory method for instantiating and initializing this object.
+   */
+  static std::unique_ptr<DutLib> Create(const std::string& fpga);
+  /**
+   * Forbids copies or assignments of DutLib.
+   */
+  DutLib(const DutLib&) = delete;
+  DutLib& operator=(const DutLib&) = delete;
+  /**
+   * Calls opentitanlib backend transport init for FPGA.
+   */
+  void DutFpgaLoadBitstream(const std::string& fpga_bitstream);
+  /**
+   * Calls opentitanlib test util to load an SRAM ELF into the DUT over JTAG.
+   */
+  void DutLoadSramElf(const std::string& openocd, const std::string& elf);
+  /**
+   * Calls opentitanlib test util to wait for a message over the SPI console.
+   */
+  void DutConsoleWaitForRx(const char*, uint64_t timeout_ms);
+  /**
+   * Calls opentitanlib test util to receive a message over the SPI console.
+   */
+  std::string DutConsoleRx(bool quiet, uint64_t timeout_ms);
+  /**
+   * Calls opentitanlib test util to send a message over the SPI console.
+   */
+  void DutConsoleTx(std::string& msg);
+
  private:
   // Must match the opentitanlib UartConsole buffer size defined here:
   // https://github.com/lowRISC/opentitan/blob/673199e30f85db799df6a31c983e8e41c8afb6c8/sw/host/opentitanlib/src/uart/console.rs#L46
   static constexpr size_t kMaxRxMsgSizeInBytes = 16384;
 
+  // Force users to call `Create` factory method.
+  DutLib(void* transport) : transport_(transport){};
+
   void* transport_;
   char console_msg_buf_[kMaxRxMsgSizeInBytes];
-
-  // Force users to call `Create` factory method.
-  DutLib(void* transport) : transport_(transport) {};
-
- public:
-  // Forbids copies or assignments of DutLib.
-  DutLib(const DutLib&) = delete;
-  DutLib& operator=(const DutLib&) = delete;
-
-  static std::unique_ptr<DutLib> Create(const std::string& fpga);
-
-  // Calls opentitanlib backend transport init for FPGA.
-  void DutFpgaLoadBitstream(const std::string& fpga_bitstream);
-
-  // Calls opentitanlib test util to load an SRAM ELF into the DUT over JTAG.
-  void DutLoadSramElf(const std::string& openocd, const std::string& elf);
-
-  // Calls opentitanlib test util to wait for a message over the SPI console.
-  void DutConsoleWaitForRx(const char*, uint64_t timeout_ms);
-
-  // Calls opentitanlib test util to receive a message over the SPI console.
-  std::string DutConsoleRx(bool quiet, uint64_t timeout_ms);
-
-  // Calls opentitanlib test util to send a message over the SPI console.
-  void DutConsoleTx(std::string& msg);
 };
 
 }  // namespace test_programs
