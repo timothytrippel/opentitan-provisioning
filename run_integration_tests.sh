91fde5be9e0568fe5090aa789e234fb9e7c47d72
Miguel Osorio <miguelosorio@google.com>
2025-03-14 11:30:32 -0700

    [github] Add loadtest with staging HSM.
    Adds Provisioning Appliance load test targeting the staging HSM
    environment. We refer to staging as `prod` as of now.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
+set -x
 
 # Parse command line options.
 for i in "$@"; do
@@ -25,17 +26,27 @@ for i in "$@"; do
   esac
 done
 
-if [[ -n "${OT_PROV_PROD_EN}" ]]; then
-  # Spawn the SPM server as a process and store its process ID.
-  echo "Launching SPM server outside of container"
-  . config/prod/env/spm.env
-  bazelisk run //src/spm:spm_server -- \
-    --port=5000 \
-    "--hsm_so=${HSMTOOL_MODULE}" \
-    --spm_config_dir=${OPENTITAN_VAR_DIR}/config/prod/spm &
-  SPM_COMMAND_PID=$!
-fi
+# SPM_PID_FILE is used to store the process ID of the SPM server process.
+# This is used to send a kill signal to the process when the script exits.
+# The variable is only set if the --prod flag is passed.
+SPM_PID_FILE="/tmp/spm.pid"
 
+# spm_server_try_stop sends a kill signal to the SPM server process if it is
+# running. It also waits for the process to terminate and removes the PID file.
+# This function is idempotent and can be called multiple times.
+spm_server_try_stop() {
+  if [ -f "${SPM_PID_FILE}" ]; then
+    SPM_PID=$(cat "${SPM_PID_FILE}")
+    kill "${SPM_PID}" 2>/dev/null || true
+    wait "${SPM_PID}" 2>/dev/null || true
+    rm "${SPM_PID_FILE}"
+  fi
+}
+
+# Unconditionally stop and remove the pod if it exists.
+# The --ignore flag is used to suppress errors if the pod does not exist.
+podman pod stop provapp --ignore
+podman pod rm provapp --ignore
 
 # Register trap to shutdown containers before exit.
 # Teardown containers. This currently does not remove the container volumes.
@@ -47,16 +58,26 @@ shutdown_callback() {
 
   # Send kill signal to SPM server process and wait for it to terminate.
   if [[ -n "${OT_PROV_PROD_EN}" ]]; then
-    kill "${SPM_COMMAND_PID}" 2>/dev/null
-    wait "${SPM_COMMAND_PID}" 2>/dev/null
+    spm_server_try_stop
   fi
 }
 trap shutdown_callback EXIT
 
-
 # Build and deploy containers. The ${OT_PROV_PROD_EN} envar is checked
 # by `deploy_test_k8_pod.sh`.
 ./util/containers/deploy_test_k8_pod.sh
+
+if [[ -n "${OT_PROV_PROD_EN}" ]]; then
+  # Spawn the SPM server as a process and store its process ID.
+  echo "Launching SPM server outside of container"
+  . config/prod/env/spm.env
+  spm_server_try_stop
+  bazelisk run //src/spm:spm_server -- \
+    --port=5000 \
+    "--hsm_so=${HSMTOOL_MODULE}" \
+    "--spm_config_dir=${OPENTITAN_VAR_DIR}/config/prod/spm" &
+  echo $! > "${SPM_PID_FILE}"
+fi
 
 # Run the loadtest.
 echo "Running PA loadtest ..."
