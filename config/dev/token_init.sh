0b3c3dc3ab294fa6342070a31ef84e44d4538b07
Miguel Osorio <miguelosorio@google.com>
2025-04-01 22:26:21 -0700

    [dev] split offline/spm HSM init scripts.
    Split offline and SPM HSM initialization scripts. Each script takes an
    input (-i) and an output (-o) argument to facilitate sharing of files
    between HSM initialization stages.
    
    This script also adds bazel targets to package the scripts and their
    dependencies into tar files.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- config/dev/token_init.sh
+++ config/dev/token_init.sh
@@ -9,10 +9,7 @@ CONFIG_DIR="$(realpath "$(dirname "$0")")"
 
 source "${CONFIG_DIR}/env/spm.env"
 
-SKU_CONFIG_FILES=(
-  "${CONFIG_DIR}/spm/sku/hsm_spm_init.sh"
-  "${CONFIG_DIR}/spm/sku/sival/hsm_sku_init.sh"
-)
+export HSMTOOL_BIN="${OPENTITAN_VAR_DIR}/bin/hsmtool"
 
 # Check token initialization dependencies.
 if [ -z "${OPENTITAN_VAR_DIR}" ]; then
@@ -25,8 +22,8 @@ if [ ! -d "${OPENTITAN_VAR_DIR}" ]; then
   return 1
 fi
 
-if [ ! -x "${OPENTITAN_VAR_DIR}/bin/hsmtool" ]; then
-  echo "Error: '${OPENTITAN_VAR_DIR}/bin/hsmtool' is not executable or does not exist."
+if [ ! -x "${HSMTOOL_BIN}" ]; then
+  echo "Error: '${HSMTOOL_BIN}' is not executable or does not exist."
   return 1
 fi
 
@@ -48,9 +45,25 @@ function run_hsm_init() {
     return 1
   }
 
+  shift
+
   echo "Running HSM initialization script: ${init_script}"
-  "${init_script}"
+  "${init_script}" "$@"
 }
+
+# Run the HSM initialization script for SPM.
+run_hsm_init "${CONFIG_DIR}/spm/sku/hsm_spm_init.sh" \
+  -o "${CONFIG_DIR}/spm/sku/spm_hsm_init.tar.gz"
+
+# Run the SKU initilization script in the offline HSM partition.
+run_hsm_init "${CONFIG_DIR}/spm/sku/sival/hsm_offline_init.sh" \
+  -i "${CONFIG_DIR}/spm/sku/spm_hsm_init.tar.gz" \
+  -o "${CONFIG_DIR}/spm/sku/sival/hsm_offline_init.tar.gz"
+
+# Run the SKU initialization script in the SPM partition.
+run_hsm_init "${CONFIG_DIR}/spm/sku/sival/hsm_spm_init.sh" \
+  -i "${CONFIG_DIR}/spm/sku/sival/hsm_offline_init.tar.gz" \
+  -o "${CONFIG_DIR}/spm/sku/sival/hsm_sival_sku.tar.gz"
 
 for filename in "${SKU_CONFIG_FILES[@]}"; do
   echo "Processing file: ${filename}"
