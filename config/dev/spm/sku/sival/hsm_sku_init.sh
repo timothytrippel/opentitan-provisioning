26c7430d10052ae62aae0f0f6cb90c702e2b4989
Miguel Osorio <miguelosorio@google.com>
2025-04-01 21:15:31 -0700

    [dev] Generate SKU specific CA certificates.
    This change adds root and intermediate CA certificates for the SiVal SKU
    in the `DEV` environment. OpenSSL is used to generate CSRs and final
    certificates.
    
    The output from the `hsm_sku_init.sh` script is now under the following
    subdirs:
    
    * `hsm`: HSM intermediate outputs, i.e. wrapped keys exported from the
      offline HSM and imported into the SPM HSM.
    * `ca`: SKU root and intermediate CAs.
    * `pub`: SKU public keys.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- config/dev/spm/sku/sival/hsm_sku_init.sh
+++ config/dev/spm/sku/sival/hsm_sku_init.sh
@@ -6,10 +6,50 @@
 set -e
 
 # The script must be executed from its local directory.
+readonly OUTDIR_HSM="hsm"
+readonly OUTDIR_CA="ca"
+readonly OUTDIR_PUB="pub"
+
+# certgen generates a certificate for the given config file and signs it with
+# the given CA key.
+certgen () {
+  config_basename=$1
+  ca_key=$2
+  endorsing_key=$3
+
+  # Generate the CA certificate for the root CA.
+  SOFTHSM2_CONF="${SOFTHSM2_CONF_OFFLINE}" \
+  PKCS11_MODULE_PATH="${HSMTOOL_MODULE}" \
+  openssl req -new -engine pkcs11 -keyform engine \
+    -config "${config_basename}.conf" \
+    -out "${OUTDIR_CA}/${config_basename}.csr" \
+    -key "pkcs11:pin-value=${HSMTOOL_PIN};object=${ca_key}"
+
+  SOFTHSM2_CONF="${SOFTHSM2_CONF_OFFLINE}" \
+  PKCS11_MODULE_PATH="${HSMTOOL_MODULE}" \
+  openssl x509 -req -engine pkcs11 -keyform engine \
+    -in "${OUTDIR_CA}/${config_basename}.csr" \
+    -out "${OUTDIR_CA}/${config_basename}.pem" \
+    -days 3650 \
+    -extfile "${config_basename}.conf" \
+    -extensions v3_ca \
+    -signkey "pkcs11:pin-value=${HSMTOOL_PIN};object=${endorsing_key}"
+}
+
+# Create output directory for HSM exported files.
+mkdir -p "${OUTDIR_HSM}" "${OUTDIR_CA}" "${OUTDIR_PUB}"
+
+echo "Configuring Offline HSM"
 SOFTHSM2_CONF="${SOFTHSM2_CONF_OFFLINE}" "${OPENTITAN_VAR_DIR}/bin/hsmtool" \
   --logging=info                     \
   --token="${SPM_HSM_TOKEN_OFFLINE}" \
   exec "01.hsm_offline.hjson"
+
+echo "Generating CA certificates"
+certgen ca_root opentitan-ca-root-v0.priv opentitan-ca-root-v0.priv
+certgen ca_int_dice sival-dice-key-p256-v0.priv opentitan-ca-root-v0.priv
+
+tar -czvf sival.tar.gz "${OUTDIR_HSM}" "${OUTDIR_CA}" "${OUTDIR_PUB}"
 
 "${OPENTITAN_VAR_DIR}/bin/hsmtool" \
   --logging=info                   \
